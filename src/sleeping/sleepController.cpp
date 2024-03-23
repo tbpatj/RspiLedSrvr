b448ceb8f6c71d8361833055106e01a24dbd3333
Timothy Luebke <luebketj@gmail.com>
2024-03-23 14:57:14 -0600

    feat: updated so now every 2 seconds the sleepcontroller checks if the capture device is still asleep or lost signal

--- src/sleeping/sleepController.cpp
+++ src/sleeping/sleepController.cpp
@@ -3,8 +3,20 @@
 class SleepController {
     std::vector<Sleeper> sleepers;
     int state = 0;
+    std::chrono::milliseconds last_check = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
     public: 
     void update() {
+        std::chrono::milliseconds now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+        //check every 2 seconds if the tv signal is still lost or the picture is still sleeping
+        if((now - last_check).count() > 2000 && !using_webcam){
+            last_check = now;
+            //check if the tv signal is lost
+            if(tv_no_signal || tv_sleeping){
+                captureDevice.updateFrame();
+            }
+        } else if(using_webcam) {
+            last_check = now;
+        }
         //when there is no tv signal and we haven't slept all the devices, go through and sleep all devices
         if(tv_no_signal && state == 0){
             std::cout << "Sleeping" << std::endl;
